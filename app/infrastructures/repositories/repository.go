1dff2a407d00c01a938c559361d7f53d5da765cf
2814109 <2814109k@gmail.com>
2023-07-12 08:36:51 +0900

    commit

--- app/infrastructures/repositories/repository.go
+++ app/infrastructures/repositories/repository.go
@@ -1,4 +1,5 @@
 package repositories
 
 type Repository interface {
+	FindAllTodo()
 }
---
+++ app/infrastructures/repositories/todos_repository.go
@@ -0,0 +1,10 @@
+package repositories
+
+import (
+	"context"
+	models "my_gql_server/my_models"
+)
+
+func FindAllTodo(ctx context.Context) (models.TodoSlice, error) {
+	return nil, nil
+}
