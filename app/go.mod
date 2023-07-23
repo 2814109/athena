61775bb131f10b71dc8ded85ed772ba1a8644ef8
2814109 <2814109k@gmail.com>
2023-07-24 00:27:27 +0900

    set CORS

--- app/go.mod
+++ app/go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/99designs/gqlgen v0.17.31
 	github.com/brianvoe/gofakeit/v6 v6.23.0
 	github.com/friendsofgo/errors v0.9.2
+	github.com/go-chi/chi v1.5.4
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/rs/cors v1.9.0
 	github.com/samber/lo v1.38.1
 	github.com/vektah/gqlparser/v2 v2.5.1
 	github.com/volatiletech/null/v8 v8.1.2
@@ -22,7 +24,6 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/rs/cors v1.9.0 // indirect
 	github.com/volatiletech/randomize v0.0.1 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
@@ -33,7 +34,7 @@ require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/go-playground/validator/v10 v10.14.1
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.0
 	github.com/hashicorp/golang-lru/v2 v2.0.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
--- app/server.go
+++ app/server.go
@@ -5,22 +5,37 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/go-chi/chi"
+
 	"my_gql_server/graph"
 
 	"my_gql_server/presentations"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/websocket"
+
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/volatiletech/sqlboiler/v4/boil"
-
 	"github.com/rs/cors"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 const defaultPort = "8888"
 
+const clientSideHost = "http://localhost:3000"
+
 func main() {
+
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{clientSideHost},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -35,14 +50,27 @@ func main() {
 	boil.SetDB(connectDB)
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &presentations.Resolver{}}))
-	mux := http.NewServeMux()
-	handler := cors.Default().Handler(mux)
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	srv.AddTransport(&transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				// Check against your desired domains here
+				return r.Host == "http://127.0.0.1"
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		},
+	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+	})
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-	log.Fatal(http.ListenAndServe(":"+port, handler))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
