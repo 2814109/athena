de7e5220fe8c140dda44b4f317ad98683786582a
2814109 <2814109k@gmail.com>
2023-07-31 00:32:12 +0900

    add RHF

--- client/remix/app/routes/dashboard.predict-cost.tsx
+++ client/remix/app/routes/dashboard.predict-cost.tsx
@@ -1,7 +1,28 @@
 import { useUser } from "@clerk/remix";
+import { useForm } from "react-hook-form";
 
 export default function PredictCostPage() {
   const { isLoaded, isSignedIn, user } = useUser();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
 
-  return <h1>hello : {user?.id}</h1>;
+  return (
+    <>
+      <h1>hello : {user?.id}</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="default value of test" {...register("example")} />
+        <br />
+        <input {...register("exampleRequired", { required: true })} />
+        {errors.exampleRequired && <span>This field is required</span>}
+        <br />
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
