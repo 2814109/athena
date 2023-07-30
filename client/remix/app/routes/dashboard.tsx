155924f7ee4d6b51cf6fb6800fe8515bf9bbd6ef
2814109 <2814109k@gmail.com>
2023-07-31 00:56:35 +0900

    create header

--- client/remix/app/routes/dashboard.tsx
+++ client/remix/app/routes/dashboard.tsx
@@ -8,6 +8,8 @@ import { useUser } from "@clerk/remix";
 import { getAuth } from "@clerk/remix/ssr.server";
 import { LoaderFunction, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import { css } from "styled-system/css";
+import { flex } from "styled-system/patterns";
 
 export const loader: LoaderFunction = async (args) => {
   const { userId } = await getAuth(args);
@@ -18,28 +20,44 @@ export const loader: LoaderFunction = async (args) => {
 };
 
 export default function Index() {
-  const { isLoaded, isSignedIn, user } = useUser();
-
-  if (!isLoaded || !isSignedIn) {
-    return null;
-  }
-
   return (
     <div>
-      <div>Hello, {user.firstName} welcome to Clerk</div>
-      <br />
-      <SignedIn>
-        <h1>Index route</h1>
-        <p>You are signed in!</p>
-        <UserButton />
-      </SignedIn>
-      <br />
-
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-
+      <Header />
       <Outlet />
     </div>
   );
 }
+
+const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <>
+      <SignedIn>
+        <div
+          className={css({
+            padding: "0.5rem 1rem",
+            display: "flex",
+            justifyContent: "space-between",
+          })}
+        >
+          <h1>title</h1>
+          <div
+            className={css({
+              display: "flex",
+              alignItems: "center",
+              gap: "1rem",
+              justifyContent: "end",
+            })}
+          >
+            {user?.firstName}
+            <UserButton />
+          </div>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  );
+};
