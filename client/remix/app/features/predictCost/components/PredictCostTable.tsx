bee6b06c1728a7d608e6297e3213a38519e1e7b6
2814109 <2814109k@gmail.com>
2023-08-03 00:05:43 +0900

    total cost

--- client/remix/app/features/predictCost/components/PredictCostTable.tsx
+++ client/remix/app/features/predictCost/components/PredictCostTable.tsx
@@ -2,9 +2,18 @@ import { useGetAllPredictCost } from "../hooks/useAllPredictCost";
 
 export const PredictCostTable = () => {
   const { predictCosts } = useGetAllPredictCost();
+  const initialValue = 0;
+
+  const totalCounts = predictCosts
+    ?.map(({ Amount }) => Amount)
+    .reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      initialValue
+    );
   return (
     <>
       <h1>Table Component</h1>
+      <h2>Total : {`${totalCounts}`}</h2>
       {predictCosts?.map((predictCost) => (
         <div>{JSON.stringify(predictCost)}</div>
       ))}
