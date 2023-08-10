a3c3176ed301dceaa1b77a6f6bdbf871a0c1cc18
2814109 <2814109k@gmail.com>
2023-08-11 00:06:11 +0900

    commit

---
+++ client/remix/app/features/monthly/components/Graph/index.tsx
@@ -0,0 +1,46 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  Legend,
+} from "recharts";
+
+type Props = {
+  payments:
+    | {
+        __typename?: "Payment" | undefined;
+        id: string;
+        label: string;
+        categoryName: string;
+        cost: number;
+        paymentType: string;
+        paymentAt: any;
+      }[]
+    | undefined;
+};
+export const Graph = ({ payments }: Props) => {
+  return (
+    <BarChart
+      width={800}
+      height={300}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+      barSize={20}
+      data={payments}
+    >
+      <XAxis dataKey="paymentAt" padding={{ left: 10, right: 10 }} />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <CartesianGrid strokeDasharray="4 6" />
+      <Bar dataKey="cost" fill="#8884d8" />
+    </BarChart>
+  );
+};
--- client/remix/app/features/monthly/components/Table/PaymentTable.tsx
+++ client/remix/app/features/monthly/components/Table/PaymentTable.tsx
@@ -1,13 +1,5 @@
 import { useGetAllPayment } from "../../hooks/useGetAllPayment";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-  Legend,
-} from "recharts";
+import { Graph } from "../Graph";
 
 export const PaymentTable = () => {
   const { payments } = useGetAllPayment();
@@ -26,25 +18,7 @@ export const PaymentTable = () => {
       <h1>Table Component</h1>
       <h2>Total : {`${totalCounts}`}</h2>
       <>
-        <BarChart
-          width={800}
-          height={300}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          barSize={20}
-          data={payments}
-        >
-          <XAxis dataKey="paymentAt" padding={{ left: 10, right: 10 }} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <CartesianGrid strokeDasharray="4 6" />
-          <Bar dataKey="cost" fill="#8884d8" />
-        </BarChart>
+        <Graph payments={payments} />
       </>
     </>
   );
