fa8f85f9c32bfe79ead4bb4a34dc53c8ba0f9868
2814109 <2814109k@gmail.com>
2023-08-11 17:30:14 +0900

    rm tanstack table

--- client/remix/app/features/monthly/components/Graph/index.tsx
+++ client/remix/app/features/monthly/components/Graph/index.tsx
@@ -1,7 +1,38 @@
 import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 import { PaymentsType } from "../../types/PaymentsType";
+import { getAllDatesInMonth } from "~/libs/getAllDatesInMonth";
+import { isDatesEqual } from "~/libs/isDatesEqual";
 
 export const Graph = ({ payments }: PaymentsType) => {
+  const today = new Date();
+  const targetYear = today.getFullYear();
+  const targetMonth = today.getMonth() + 1;
+
+  const allDatesInMonth = getAllDatesInMonth(targetYear, targetMonth);
+
+  const datesDataset = allDatesInMonth.map((element) => ({
+    date: element,
+    count: 0,
+  }));
+
+  const dataSet = datesDataset.map((dateObject) => {
+    const countByDate = payments
+      ?.filter(({ paymentAt }) =>
+        isDatesEqual(dateObject.date, new Date(paymentAt))
+      )
+      .map(({ cost }) => cost)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const formatDate = (date: Date): string =>
+      `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+
+    return {
+      date: formatDate(dateObject.date),
+      count: countByDate,
+    };
+  });
+
+  console.log(dataSet);
   return (
     <BarChart
       width={800}
@@ -13,13 +44,13 @@ export const Graph = ({ payments }: PaymentsType) => {
         bottom: 5,
       }}
       barSize={20}
-      data={payments}
+      data={dataSet}
     >
-      <XAxis dataKey="paymentAt" padding={{ left: 10, right: 10 }} />
+      <XAxis dataKey="date" padding={{ left: 10, right: 10 }} />
       <YAxis />
       <Tooltip />
       <CartesianGrid strokeDasharray="4 6" />
-      <Bar dataKey="cost" fill="#8884d8" />
+      <Bar dataKey="count" fill="#8884d8" />
     </BarChart>
   );
 };
--- client/remix/app/features/monthly/components/Table/index.tsx
+++ client/remix/app/features/monthly/components/Table/index.tsx
@@ -1,37 +1,8 @@
-import { isDatesEqual } from "~/libs/isDatesEqual";
 import { PaymentsType } from "../../types/PaymentsType";
 import { PaymentRow } from "./row";
 import { Payment } from "~/gql/graphql";
 
-import { getAllDatesInMonth } from "~/libs/getAllDatesInMonth";
-
 export const Table = ({ payments }: PaymentsType) => {
-  const targetYear = 2023; // 対象の年
-  const targetMonth = 8; // 対象の月 (1 月: 1, 2 月: 2, ... 12 月: 12)
-
-  const allDatesInMonth = getAllDatesInMonth(targetYear, targetMonth);
-
-  const datesDataset = allDatesInMonth.map((element) => ({
-    date: element,
-    count: 0,
-  }));
-
-  const dataSet = datesDataset.map((dateObject) => {
-    const countByDate = payments
-      ?.filter(({ paymentAt }) =>
-        isDatesEqual(dateObject.date, new Date(paymentAt))
-      )
-      .map(({ cost }) => cost)
-      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-
-    return {
-      date: dateObject.date,
-      count: countByDate,
-    };
-  });
-
-  console.log(dataSet);
-
   return (
     <>
       {payments?.map((payment) => (
