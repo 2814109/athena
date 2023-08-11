2901d776fa89a527f4d82ed1540e536ce3a5cacc
2814109 <2814109k@gmail.com>
2023-08-11 23:10:43 +0900

    update

--- client/remix/app/features/monthly/components/Modal/index.tsx
+++ client/remix/app/features/monthly/components/Modal/index.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import { MonthlyForm } from "../Form/MonthlyForm";
+import { Button, ButtonToolbar, Animation } from "rsuite";
 
 export const Modal = () => {
   const [isOpen, setIsOpen] = useState<boolean>(true);
 
   return (
     <>
-      <button onClick={() => setIsOpen((pre) => !pre)}>
-        {!isOpen ? "open" : "close"}
-      </button>
+      <ButtonToolbar>
+        <Button
+          appearance="primary"
+          block
+          onClick={() => setIsOpen((pre) => !pre)}
+        >
+          {!isOpen ? "open" : "close"}
+        </Button>
+      </ButtonToolbar>
+      <div style={{ height: "24px" }} />
       {isOpen && <MonthlyForm />}
     </>
   );
