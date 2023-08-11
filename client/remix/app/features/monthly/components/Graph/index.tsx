a5fbab12da8fd7455bad7a69c624be35a076c071
2814109 <2814109k@gmail.com>
2023-08-11 22:05:10 +0900

    update

--- client/remix/app/features/monthly/components/Graph/index.tsx
+++ client/remix/app/features/monthly/components/Graph/index.tsx
@@ -72,7 +72,7 @@ export const Graph = ({ payments, totalCounts }: Props) => {
 
   return (
     <>
-      <h2>prediction : ¥{Math.ceil(restCost + (totalCounts ?? 0))}</h2>
+      <h2>(prediction : ¥{Math.ceil(restCost + (totalCounts ?? 0))})</h2>
       <ComposedChart
         width={800}
         height={300}
