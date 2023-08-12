f798258d59d3ca6b9f0d420931005e90679dbcf5
2814109 <2814109k@gmail.com>
2023-08-12 14:53:10 +0900

    commit

--- client/remix/app/features/predictCost/components/Modal.tsx
+++ client/remix/app/features/predictCost/components/Modal.tsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import { CreatePredictCostForm } from "./Form/CreatePredictCostForm";
+import { Modal, Button, ButtonToolbar } from "rsuite";
+import { Spacer } from "~/components/Spacer";
 
-export const Modal = () => {
+export const ModalContainer = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleClose = () => setIsOpen(false);
 
   return (
     <>
-      <button onClick={() => setIsOpen((pre) => !pre)}>
-        {!isOpen ? "open" : "close"}
-      </button>
-      {isOpen && <CreatePredictCostForm />}
+      <ButtonToolbar>
+        <Button
+          appearance="primary"
+          block
+          onClick={() => setIsOpen((pre) => !pre)}
+        >
+          {!isOpen ? "open" : "close"}
+        </Button>
+      </ButtonToolbar>
+      <div style={{ height: "24px" }} />
+
+      <Modal open={isOpen} onClose={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Predict Cost</Modal.Title>
+        </Modal.Header>
+        <Spacer size={24} />
+        <CreatePredictCostForm />
+      </Modal>
     </>
   );
 };
--- client/remix/app/routes/dashboard.predict-cost.tsx
+++ client/remix/app/routes/dashboard.predict-cost.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
 import { Spinner } from "~/components/Spinner";
-import { Modal } from "~/features/predictCost/components/Modal";
+import { ModalContainer } from "~/features/predictCost/components/Modal";
 import { PredictCostStatistics } from "~/features/predictCost/components/PredictCostStatistics";
 
 export default function PredictCostPage() {
   return (
     <>
       <Suspense fallback={<Spinner />}>
-        <Modal />
+        <ModalContainer />
         <PredictCostStatistics />
       </Suspense>
     </>
