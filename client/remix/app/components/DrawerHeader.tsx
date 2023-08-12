21b26bae496688f9e8f3c75fe9c8675e1260f9c0
2814109 <2814109k@gmail.com>
2023-08-12 12:36:42 +0900

    update

---
+++ client/remix/app/components/DrawerHeader.tsx
@@ -0,0 +1,24 @@
+import { DispatchWithoutAction } from "react";
+import { Drawer, Placeholder } from "rsuite";
+import { TypeAttributes } from "rsuite/esm/@types/common";
+import { Header } from "./Header";
+
+type Props = {
+  handleClick: DispatchWithoutAction;
+  openState: boolean;
+  placement: TypeAttributes.Placement4 | undefined;
+};
+
+export const DrawerHeader = ({ handleClick, openState, placement }: Props) => (
+  <Drawer placement={placement} open={openState} onClose={handleClick}>
+    <Drawer.Header>
+      <Drawer.Title>
+        <Header />
+      </Drawer.Title>
+      <Drawer.Actions></Drawer.Actions>
+    </Drawer.Header>
+    <Drawer.Body>
+      <Placeholder.Paragraph rows={8} />
+    </Drawer.Body>
+  </Drawer>
+);
--- client/remix/app/routes/dashboard.tsx
+++ client/remix/app/routes/dashboard.tsx
@@ -1,10 +1,12 @@
 import { getAuth } from "@clerk/remix/ssr.server";
 import { LoaderFunction, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
-import { Suspense } from "react";
-import { Header } from "~/components/Header";
+import { Suspense, useState } from "react";
 import { MainContainer } from "~/styles/MainContainer";
 import styles from "rsuite/dist/rsuite.min.css";
+import { IconButton, ButtonToolbar } from "rsuite";
+import { TypeAttributes } from "rsuite/esm/@types/common";
+import { DrawerHeader } from "~/components/DrawerHeader";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
@@ -19,9 +21,27 @@ export const loader: LoaderFunction = async (args) => {
 };
 
 export default function Index() {
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState<TypeAttributes.Placement4>();
+  const handleOpen = (key: TypeAttributes.Placement4) => {
+    setOpen(true);
+    setPlacement(key);
+  };
+
+  const handleClose = () => {
+    setOpen((pre) => !pre);
+  };
   return (
     <>
-      <Header />
+      <ButtonToolbar>
+        <IconButton onClick={() => handleOpen("top")}>Top</IconButton>
+      </ButtonToolbar>
+      <DrawerHeader
+        handleClick={handleClose}
+        openState={open}
+        placement={placement}
+      />
+
       <MainContainer>
         <Suspense fallback={<>Loading</>}>
           <div>
