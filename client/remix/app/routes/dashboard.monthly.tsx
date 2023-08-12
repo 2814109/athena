89387af1715cd9d62997b38e1b3217674e78ed9b
2814109 <2814109k@gmail.com>
2023-08-13 02:21:55 +0900

    update

--- client/remix/app/routes/dashboard.monthly.tsx
+++ client/remix/app/routes/dashboard.monthly.tsx
@@ -3,18 +3,33 @@ import { css } from "styled-system/css";
 import { Spinner } from "~/styles/Spinner";
 import { ModalContainer } from "~/features/monthly/components/Modal/index";
 import { Summary } from "~/features/monthly/components/Summary";
+import { Uploader } from "rsuite";
 
 export default function MonthlyPage() {
   return (
-    <Suspense fallback={<Spinner />}>
-      <div
-        className={css({
-          width: "800px",
-        })}
-      >
-        <ModalContainer />
-      </div>
-      <Summary />
-    </Suspense>
+    <>
+      <Uploader action="//jsonplaceholder.typicode.com/posts/" draggable>
+        <div
+          style={{
+            height: 150,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <span>Click or Drag files to this area to upload</span>
+        </div>
+      </Uploader>{" "}
+      <Suspense fallback={<Spinner />}>
+        <div
+          className={css({
+            width: "800px",
+          })}
+        >
+          <ModalContainer />
+        </div>
+        <Summary />
+      </Suspense>
+    </>
   );
 }
