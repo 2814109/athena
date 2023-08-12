60570768306ea8332d4a8bf54a45be033ccdfd40
2814109 <2814109k@gmail.com>
2023-08-12 15:37:12 +0900

    commit

--- client/remix/app/components/DrawerHeader.tsx
+++ client/remix/app/components/DrawerHeader.tsx
@@ -1,7 +1,10 @@
-import { DispatchWithoutAction } from "react";
-import { Button, Drawer, Placeholder } from "rsuite";
+import { Dispatch, DispatchWithoutAction, useEffect, useState } from "react";
+import { Button, Drawer } from "rsuite";
 import { TypeAttributes } from "rsuite/esm/@types/common";
 import { Header } from "./Header";
+import { Nav } from "rsuite";
+import HomeIcon from "@rsuite/icons/legacy/Home";
+import { useLocation, useNavigate } from "@remix-run/react";
 
 type Props = {
   handleClick: DispatchWithoutAction;
@@ -9,16 +12,61 @@ type Props = {
   placement: TypeAttributes.Placement4 | undefined;
 };
 
-export const DrawerHeader = ({ handleClick, openState, placement }: Props) => (
-  <Drawer placement={placement} open={openState} onClose={handleClick}>
-    <Drawer.Header closeButton={false}>
-      <Drawer.Actions>
-        <Button onClick={handleClick}>Close</Button>
-      </Drawer.Actions>
-    </Drawer.Header>
-    <Header />
-    <Drawer.Body>
-      <Placeholder.Paragraph rows={6} />
-    </Drawer.Body>
-  </Drawer>
-);
+export const DrawerHeader = ({ handleClick, openState, placement }: Props) => {
+  const location = useLocation();
+  const [active, setActive] = useState<string>(location.pathname.split("/")[2]);
+
+  return (
+    <Drawer placement={placement} open={openState} onClose={handleClick}>
+      <Drawer.Header closeButton={false}>
+        <Drawer.Actions>
+          <Button onClick={handleClick}>Close</Button>
+        </Drawer.Actions>
+      </Drawer.Header>
+      <Header />
+      <Drawer.Body>
+        <Navbar active={active} onSelect={setActive} />
+      </Drawer.Body>
+    </Drawer>
+  );
+};
+
+const Navbar = ({
+  active,
+  onSelect,
+  ...props
+}: {
+  active: string;
+  onSelect: Dispatch<React.SetStateAction<string>>;
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <Nav
+      {...props}
+      activeKey={active}
+      onSelect={onSelect}
+      style={{ marginBottom: 50 }}
+    >
+      <Nav.Item
+        eventKey="home"
+        icon={<HomeIcon />}
+        onClick={() => navigate(`/dashboard`)}
+      >
+        Home
+      </Nav.Item>
+      <Nav.Item
+        eventKey="predict-cost"
+        onClick={() => navigate(`/dashboard/predict-cost`)}
+      >
+        Predict Cost
+      </Nav.Item>
+      <Nav.Item
+        eventKey="monthly"
+        onClick={() => navigate(`/dashboard/monthly`)}
+      >
+        monthly
+      </Nav.Item>
+    </Nav>
+  );
+};
