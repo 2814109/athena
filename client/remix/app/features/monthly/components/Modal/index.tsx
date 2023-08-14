e10bf40fa74499e65ebf68425e857b15ed2e2015
2814109 <2814109k@gmail.com>
2023-08-14 21:44:28 +0900

    commit

--- client/remix/app/features/monthly/components/Modal/index.tsx
+++ client/remix/app/features/monthly/components/Modal/index.tsx
@@ -1,9 +1,49 @@
+import { Button, Drawer, IconButton, Placeholder } from "rsuite";
 import { MonthlyForm } from "../Form/MonthlyForm";
+import AngleRightIcon from "@rsuite/icons/legacy/AngleRight";
+import PlusIcon from "@rsuite/icons/Plus";
 
 import { ModalWrapper } from "~/components/ModalWrapper";
+import { useState } from "react";
+import { TypeAttributes } from "rsuite/esm/@types/common";
 
-export const ModalContainer = () => (
-  <ModalWrapper modalTitle={"Payment"}>
-    <MonthlyForm />
-  </ModalWrapper>
-);
+export const ModalContainer = () => {
+  const [open, setOpen] = useState(false);
+  const [placement, setPlacement] = useState<
+    TypeAttributes.Placement4 | undefined
+  >();
+
+  const handleOpen = (key: TypeAttributes.Placement4 | undefined) => {
+    setOpen(true);
+    setPlacement(key);
+  };
+
+  return (
+    <>
+      <IconButton
+        icon={<AngleRightIcon />}
+        onClick={() => handleOpen("right")}
+        appearance="primary"
+      >
+        Add
+      </IconButton>
+
+      <Drawer
+        size={"sm"}
+        placement={placement}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <Drawer.Header>
+          <Drawer.Title>Drawer Title</Drawer.Title>
+          <Drawer.Actions>
+            <Button onClick={() => setOpen(false)}>Cancel</Button>
+          </Drawer.Actions>
+        </Drawer.Header>
+        <Drawer.Body>
+          <MonthlyForm />
+        </Drawer.Body>
+      </Drawer>
+    </>
+  );
+};
