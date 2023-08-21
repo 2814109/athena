6617b92a2a96bd669d17af84b023b19beb7070d2
2814109 <2814109k@gmail.com>
2023-08-22 00:41:46 +0900

    update

--- client/remix/app/components/Navbar.tsx
+++ client/remix/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Nav } from "rsuite";
-import HomeIcon from "@rsuite/icons/legacy/Home";
 import { Dispatch } from "react";
 import { useNavigate } from "@remix-run/react";
+import { NAV_INDEX } from "~/constants/NAV_INDEX";
 
 export const Navbar = ({
   active,
@@ -20,36 +20,11 @@ export const Navbar = ({
       onSelect={onSelect}
       style={{ marginBottom: 50 }}
     >
-      {/* mapでループ展開 */}
-      <Nav.Item
-        eventKey="home"
-        icon={<HomeIcon />}
-        onClick={() => navigate(`/dashboard`)}
-      >
-        Home
-      </Nav.Item>
-      <Nav.Item
-        eventKey="predict-cost"
-        onClick={() => navigate(`/dashboard/predict-cost`)}
-      >
-        Predict Cost
-      </Nav.Item>
-      <Nav.Item eventKey="income" onClick={() => navigate(`/dashboard/income`)}>
-        Income
-      </Nav.Item>
-      <Nav.Item
-        eventKey="monthly"
-        onClick={() => navigate(`/dashboard/monthly`)}
-      >
-        Monthly
-      </Nav.Item>
-
-      <Nav.Item
-        eventKey="knowledge"
-        onClick={() => navigate(`/dashboard/knowledge`)}
-      >
-        knowledge
-      </Nav.Item>
+      {NAV_INDEX.map((nav) => (
+        <Nav.Item eventKey={nav.path} onClick={() => navigate(`${nav.path}`)}>
+          {nav.title}
+        </Nav.Item>
+      ))}
     </Nav>
   );
 };
---
+++ client/remix/app/constants/NAV_INDEX.ts
@@ -0,0 +1,7 @@
+export   const NAV_INDEX = [
+    { path: "/dashboard", title: "Home" },
+    { path: "/dashboard/predict-cost", title: "Predict Cost" },
+    { path: "/dashboard/income", title: "Income" },
+    { path: "/dashboard/monthly", title: "Monthly" },
+    { path: "/dashboard/knowledge", title: "Knowledge" },
+  ];
