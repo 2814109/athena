8e874d2163475df1bd18e8ff2966e531a8fc8f8d
2814109 <2814109k@gmail.com>
2023-08-22 00:44:11 +0900

    update

--- client/remix/app/constants/NAV_INDEX.ts
+++ client/remix/app/constants/NAV_INDEX.ts
@@ -4,4 +4,6 @@ export   const NAV_INDEX = [
     { path: "/dashboard/income", title: "Income" },
     { path: "/dashboard/monthly", title: "Monthly" },
     { path: "/dashboard/knowledge", title: "Knowledge" },
-  ];
+    { path: "/dashboard/financial-assets", title: "Financial Assets" },
+];
+  
