9260d2ce0ff62f4b61e4b62b564d707b1f06f303
2814109 <2814109k@gmail.com>
2023-08-26 22:07:43 +0900

    update

--- client/next-js/app/page.tsx
+++ client/next-js/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Button } from "@nextui-org/button";
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <Button>Click me</Button>
 
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
---
+++ client/next-js/app/sample/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Button } from "@nextui-org/react";
+
+export default function Index() {
+  return <Button>Click me</Button>;
+}
